819010e4c875cafef34ab4ace6434a98f38e8458
aguillonlei-alt <aguillonlei@gmail.com>
2026-01-25 23:27:48 +0800

    Implement heat index calculation for low temperatures
    Add heat index calculation for temperatures below 26.7°C.

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ dht_device = adafruit_dht.DHT11(DHT_PIN)
 
 def calculate_heat_index(T_c, R):
     if T_c is None or R is None: return None
+    
+    if T_c < 26.7:
+        return T_c
+        
     T = T_c * 1.8 + 32
     c1, c2, c3 = -42.379, 2.04901523, 10.14333127
     c4, c5, c6 = -0.22475541, -6.83783e-3, -5.481717e-2
